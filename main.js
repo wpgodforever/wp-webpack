e31e32f6405510d32f40b503ce1cdda6f946a171
wuping <wuping@xinli001.com>
2023-05-30 15:02:52 +0800

    配置css-loader

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import count from "./src/count";
 import add from "./src/add";
+import "./css/style.css";
 console.log(count(1,2));
 console.log(add(1,2));
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
     entry: './main.js',
     output: {
         filename: 'output.js',      // 打包后的文件名称
-        path: path.resolve(__dirname,'dist')  // 打包后的目录
-    }
+        path: path.resolve(__dirname, 'dist')  // 打包后的目录
+    },
+    module: {
+        rules: [
+            {
+                // 用来匹配 .css 结尾的文件
+                test: /\.css$/,
+                // use 数组里面 Loader 执行顺序是从右到左
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
